d78be082dfbadda4b479d4aaa2fde922ffcbb437
OMouad <71100063+OMouad@users.noreply.github.com>
2021-01-13 15:05:40 +0100

    Created the main methods and an inner class of KeyAdapter to receive keyboard events

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,5 +1,15 @@
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
-    
+    GameFrame(){
+        this.add(new GamePanel());
+        this.setTitle("Snake game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //The pack() method is defined in Window class in Java and it sizes the frame so that all its contents are at or above their preferred sizes.
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        //This makes the window appear in the middle of the screen
+        
+    }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,11 +1,28 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JPanel;
-public class GamePanel extends JPanel implements ActionListener{
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
+public class GamePanel extends JPanel implements ActionListener{
+    
+    GamePanel(){
+    }
+    public void startGame(){
+    }
+    @Override
+    public void paintComponent(Graphics g){}
+    public void draw(Graphics g){}
+    public void move(){}
+    public void checkApple(){}
+    public void checkCollisions(){}
+    public void gameOver(Graphics g){}
     @Override
     public void actionPerformed(ActionEvent ae) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    public class MykeyAdapter extends KeyAdapter{
     
+        public void keyPressed(KeyEvent e){
+        }
+    }
 }
