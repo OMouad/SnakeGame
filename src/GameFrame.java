bfe7d00708b4dd772d7a01d5f39477767c880f5a
OMouad <71100063+OMouad@users.noreply.github.com>
2021-01-13 16:19:07 +0100

    Changed the values of the variables and defined the startGame and paintComponent methods

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -7,6 +7,7 @@ public class GameFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //The pack() method is defined in Window class in Java and it sizes the frame so that all its contents are at or above their preferred sizes.
         this.setResizable(false);
+        this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         //This makes the window appear in the middle of the screen
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,11 +5,11 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener{
     
-    static final int SCREEN_WIDTH = 600;
-    static final int SCREEN_HEIGHT = 600;
-    static final int UNIT_SIZE = 25;//The size of the objects in the game
-    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;//The number of objects that is possible to display on the screen
-    static final int DELAY = 75;
+    static final int SCREEN_WIDTH = 1300;;
+    static final int SCREEN_HEIGHT = 750;
+    static final int UNIT_SIZE = 50;//The size of the objects in the game
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);//The number of objects that is possible to display on the screen
+    static final int DELAY = 175;
     //these arrays are going to hold all the coordinates of all the snake's bodyparts including it's head
     final int x[]= new int[GAME_UNITS]; 
     final int y[]= new int[GAME_UNITS];
@@ -25,15 +25,23 @@ public class GamePanel extends JPanel implements ActionListener{
     
     GamePanel(){
         random = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_WIDTH));
-        this.setBackground(Color.black);
-        this.setFocusable(true);
-        this.addKeyListener(new MykeyAdapter());
-        startGame();
+	this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+	this.setBackground(Color.black);
+	this.setFocusable(true);
+	this.addKeyListener(new MykeyAdapter());
+	startGame();
     }
     public void startGame(){
+	newApple();
+	running = true;
+	timer = new Timer(DELAY,this);
+	timer.start();
     }
-    public void paintComponent(Graphics g){}
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
     public void draw(Graphics g){}
     public void newApple(){
         
@@ -44,7 +52,6 @@ public class GamePanel extends JPanel implements ActionListener{
     public void gameOver(Graphics g){}
     @Override
     public void actionPerformed(ActionEvent ae) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     public class MykeyAdapter extends KeyAdapter{
     
