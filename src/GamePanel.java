7481603c42ed62799fcc3d23b9a94abe47e606e4
OMouad <71100063+OMouad@users.noreply.github.com>
2021-01-13 15:31:00 +0100

    Declaring all the variables

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,6 +5,24 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener{
     
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;//The size of the objects in the game
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;//The number of objects that is possible to display on the screen
+    static final int DELAY = 75;
+    //these arrays are going to hold all the coordinates of all the snake's bodyparts including it's head
+    final int x[]= new int[GAME_UNITS]; 
+    final int y[]= new int[GAME_UNITS];
+    //defining the initial amount of body parts of the snake
+    int bodyParts = 6;
+    int applesEaten =0;
+    int appleX,appleY; //These are the random apple's coordinates 
+    char direction ='R'; // The snake's direction
+    boolean running = false;
+    Timer timer;
+    Random random;
+    
+    
     GamePanel(){
     }
     public void startGame(){
