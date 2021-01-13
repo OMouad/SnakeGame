80103e9396d1d8fee1d7f30d4697a82c6680d230
OMouad <71100063+OMouad@users.noreply.github.com>
2021-01-13 14:49:48 +0100

    .

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,13 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import javax.swing.JFrame;
 
-/**
- *
- * @author ULTRAPC
- */
-public class GameFrame {
+public class GameFrame extends JFrame {
     
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+public class GamePanel extends JPanel implements ActionListener{
 
-/**
- *
- * @author ULTRAPC
- */
-public class GamePanel {
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
