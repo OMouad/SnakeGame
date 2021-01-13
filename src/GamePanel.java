0662b5b4d76880942e58c7b72a8d88f1fb61315f
OMouad <71100063+OMouad@users.noreply.github.com>
2021-01-13 17:23:29 +0100

    Defined the move(), checkCollisions and draw methods

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -42,16 +42,71 @@ public class GamePanel extends JPanel implements ActionListener{
         super.paintComponent(g);
         draw(g);
     }
-    public void draw(Graphics g){}
+    public void draw(Graphics g){
+        g.setColor(Color.red);
+        g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+        //We choose different color for the head of the snake and the rest of his body;
+        for(int i = 0;i< bodyParts;i++){
+            if (i==0) {
+                g.setColor(Color.green);
+            }
+            else {
+                g.setColor(new Color(45,180,0));
+            }
+            g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+        }
+    }
     public void newApple(){
+        appleX= random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY= random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
         
     }
-    public void move(){}
+    public void move(){
+        for(int i = bodyParts;i>0;i--){
+            x[i]=x[i-1];
+            y[i]=y[i-1];
+        }
+        switch(direction){
+            case 'U':
+                y[0]=y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0]=y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0]=x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0]=x[0] + UNIT_SIZE;
+                break;
+        }
+    }
     public void checkApple(){}
-    public void checkCollisions(){}
+    public void checkCollisions(){
+        // Checks if the body collides with the head
+        for (int i = bodyParts;i>0;i--){
+            if ((x[0]==x[i])&&(y[0]==y[i])){
+                running = false;
+            }
+        }
+        // Checks if the body touches a border
+        if (x[0]< 0 || x[0]> SCREEN_WIDTH ||y[0]< 0 || y[0]> SCREEN_HEIGHT){
+            running = false;
+        }
+        if (!running){
+            timer.stop();
+        }
+        
+    }
     public void gameOver(Graphics g){}
     @Override
     public void actionPerformed(ActionEvent ae) {
+        if(running){
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
     public class MykeyAdapter extends KeyAdapter{
     
